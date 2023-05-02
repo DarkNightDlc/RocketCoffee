740b76072ba42b1560ec84b123f563724192b5c5
DarkNightDLC <pedronkdarck@gmail.com>
2023-05-02 20:11:59 -0300

    Alterando a Local da loja

--- src/Pages/Lojas.tsx
+++ src/Pages/Lojas.tsx
@@ -7,7 +7,7 @@ const Lojas = ()=> {
             <Header />
             <main className="m-auto md:p-10 p-5 flex items-center justify-center h-[calc(100vh-7rem)] max-w-[1000px] ">
                 <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7476.261158470454!2d-45.9685477!3d-20.4598293!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b4138d65d0a809%3A0xff20efb235e1f6f6!2sCemig%20-%20Ag%C3%AAncia%20-%20Piumhi!5e0!3m2!1spt-BR!2sbr!4v1652734955708!5m2!1spt-BR!2sbr" 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.387287784526!2d-43.97600241336748!3d-19.86586188862029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690f71348ff6d%3A0x35b9453242d27588!2zTWluZWlyw6Nv!5e0!3m2!1spt-BR!2sbr!4v1683069016890!5m2!1spt-BR!2sbr" 
                     className="w-full h-full border-2 border-button rounded-md"
                     loading="lazy"
                     >
